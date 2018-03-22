509311c05ff9a07f0ac28ae81a63bcf52f1595bf
Tristan Crawford <tristanthtcrawford@gmail.com>
2018-03-22 14:56:58 -0500

    HelloWorld Bonuses

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -7,11 +7,21 @@ import java.io.PrintWriter;
 
 @WebServlet("/hello/*")
 public class HelloWorldServlet extends HttpServlet {
+
+    private int viewCounter;
+
+    public void init() {
+        viewCounter = 0;
+    }
+
     protected void doGet(HttpServletRequest req, HttpServletResponse res) {
         try {
+            if(req.getParameter("reset") != null)
+                viewCounter = 0;
+            viewCounter++;
             res.setContentType("text/html");
             PrintWriter out = res.getWriter();
-            out.println("<h1>Hello World!</h1>");
+            out.println(String.format("<h1>Hello %s!</h1><h2>%d Page Views!</h2>", req.getParameter("name"), viewCounter));
         } catch (IOException e) {
             System.out.println("Darn.");
         }
